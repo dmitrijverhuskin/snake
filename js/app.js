9ba303f7693a2854449116d88ed7f42b20c3ba47
Philipp Vitkovsky <philipp.vitkovsky@gmail.com>
2018-11-16 16:48:47 +0200

    fixed arrows not working

--- js/app.js
+++ js/app.js
@@ -128,9 +128,9 @@ $(document).ready(function () {
             $(document).keydown(function (e) {
                 let key = e.which;
 
-                if (key === "37" && d !== "right") d = "left";
-                    else if (key === "38" && d !== "down") d = "up";
-                    else if (key === "39" && d !== "left") d = "right";
-                    else if (key === "40" && d !== "up") d = "down";
+                if (key === 37 && d !== "right") d = "left";
+                    else if (key === 38 && d !== "down") d = "up";
+                    else if (key === 39 && d !== "left") d = "right";
+                    else if (key === 40 && d !== "up") d = "down";
             })
 });
